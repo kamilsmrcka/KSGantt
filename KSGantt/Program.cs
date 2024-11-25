9a082344415376e4125f85831c608e854c9be541
Kamil Smrčka <kamil.smrcka@gmail.com>
2024-11-25 10:02:56 +0100

    Vytvoření nového projektu a přidání Radzen komponent.

--- KSGantt/Program.cs
+++ KSGantt/Program.cs
@@ -1,11 +1,12 @@
 using KSGantt.Components;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddRadzenComponents();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
